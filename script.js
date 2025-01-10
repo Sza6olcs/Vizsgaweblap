dbc6d8865cccf436e27261624a2df5f53985a734
sza6olcs <szabolcs.balazs@outlook.com>
2025-01-10 11:49:19 +0100

    overlay kész

--- script.js
+++ script.js
@@ -30,13 +30,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
    
 
-    // Kosárba gomb funkció
-    const addToCartButtons = document.querySelectorAll('.product button');
-    addToCartButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            alert('A termék a kosárba került!');
-        });
+// Kosárba gomb funkció
+const addToCartButtons = document.querySelectorAll('.product button');
+addToCartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        alert('A termék a kosárba került!');
     });
+});
 });
    // Hamburger menü
 const hamMenu = document.querySelector(".ham-menu");
@@ -47,4 +47,28 @@ hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
 });
+    // Bezárja a menüt ha a háttérre kattintunk
+const overlay = document.querySelector(".overlay");
+
+overlay.addEventListener("click", () => {
+  hamMenu.classList.remove("active");
+  offScreenMenu.classList.remove("active");
+});
+
+// Lap tetejére gomb
+let mybutton = document.getElementById("totop");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
