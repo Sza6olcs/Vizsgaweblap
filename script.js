9d3944f492069cbecbd2a6e7f1b8526160e1c070
sza6olcs <szabolcs.balazs@outlook.com>
2025-01-15 15:06:46 +0100

    feladom

--- script.js
+++ script.js
@@ -50,11 +50,11 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
-    hamMenuButton.style.display = "none";
+    
 
   } else {
     mybutton.style.display = "none";
-    hamMenuButton.style.display = "block";
+    
   }
 }
 
